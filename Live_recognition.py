21a183727ffe921b6d1e6f0f2942931598c4cc86
T-Kingan <73171307+T-Kingan@users.noreply.github.com>
2024-03-21 13:41:20 +0000

    Adding matching

--- Live_recognition.py
+++ Live_recognition.py
@@ -10,18 +10,56 @@ img_path = 'speed_photos/UK_20mph.jpg'
 
 template = [20,30,40,50,60]
 
-for temp in template:
-    template_speed = cv2.imread('Template_speeds/'+str(temp) + '.jpeg')
-    #template_filtered = number_extract(template_speed,1)
-    template_filtered = cv2.imread('Template_speeds/'+str(temp) + 'filtered.png')
+# for temp in template:
+#     template_speed = cv2.imread('Template_speeds/'+str(temp) + '.jpeg')
+#     #template_filtered = number_extract(template_speed,1)
+#     template_filtered = cv2.imread('Template_speeds/'+str(temp) + 'filtered.png')
+
+template_speed = cv2.imread('Template_speeds/20.jpeg')
+template_filtered = cv2.imread('Template_speeds/20filtered.png')
+
+query_img = img_num(img_path)  # Image from which we want to match features
+
+#(Structural similarity?) (SSIM)
+
+def keypoints_descriptors(template, query_img, detector_name):
+    if hasattr(cv2, detector_name):
+        detector = getattr(cv2, detector_name)()
+    else:
+        print(f"Detector {detector_name} not found in cv2")
+        return
+    
+    # Find the keypoints and descriptors with detector
+    kp1, des1 = detector.detectAndCompute(template,None)
+    kp2, des2 = detector.detectAndCompute(query_img,None)
+    
+    # return the keypoints and descriptors
+    return kp1, des1, kp2, des2
+
+def match_features(template, query_img, matcher_name):
+    if hasattr(cv2, matcher_name):
+        matchor = getattr(cv2, matcher_name)()
+    else:
+        print(f"Matchor {matcher_name} not found in cv2")
+        return
+    matches = matchor.knnMatch(des1,des2,k=2)
+
+detectors = ['SIFT_create', 'ORB_create']
+matchers = ['BFMatcher']
+
+for detec in detectors:
+    print(detec)
+    keypoints_descriptors(template_filtered, query_img, detec)
+    for match in matchers:
+        print(match)
+        match_features(template_filtered, query_img, match)
 
 
-# Initilise SIFT detector
-sift = cv2.SIFT_create()
 
-# Find the keypoints and descriptors with SIFT
-kp1, des1 = sift.detectAndCompute(template_filtered,None)
-kp2, des2 = sift.detectAndCompute(img_num(img_path),None)
+
+
+
+
 
 # After obtaining the keypoints and descriptors, we can match them using a feature mapper
 # initialise Brute Force Matcher
@@ -38,10 +76,12 @@ for m,n in matches:
 
 # Draw matches
 # matching_result = cv2.drawMatches(img1, keypoints1, img2, keypoints2, good_matches, None, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
-img3 = cv2.drawMatches(template_filtered,kp1,img_num(img_path),kp2,good,None,flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+#img3 = cv2.drawMatches(template_filtered,kp1,img_num(img_path),kp2,good,None,flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+img3 = cv2.drawMatchesKnn(template_filtered, kp1, query_img, kp2, good, None, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
 
 # Display the result
 
+plt.imshow(img3), plt.show()
 
 # scene = img_num(img_path)
 # # All the 6 methods for comparison in a list
@@ -58,4 +98,13 @@ img3 = cv2.drawMatches(template_filtered,kp1,img_num(img_path),kp2,good,None,fla
 #     template_filtered = number_extract(template_speed,1)
 #     cv2.imwrite('Template_speeds/'+str(template[i]) + 'filtered.png', template_filtered)
 
+def Harris
+
+# Harris Corner Detection
+# Shi-Tomai Corner Detector
+# Scale-Invariant Feature Transform (SIFT)
+# Speeded-up robust features (SURF)
+# Histogram of Orientated Gradients
+# Binary Robust Independent Elementary Features (BRIEF)
+# Oriented FAST and Rotated BRIEF (ORB)
 
